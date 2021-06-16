0a8022eb2e26610642f8a3140aa299c3d4374185
reki2 <83732645+reki2@users.noreply.github.com>
2021-06-16 16:41:50 -0500

    Update Pigs.js
    uptdaiting pigs

--- Pigs.js
+++ Pigs.js
@@ -4,5 +4,9 @@ constructor(x,y,width,height){
    super(x,y,width,height);
     this.image = loadImage("sprites/enemy.png");
   }  
+   display(){
+console.log(this.body.speed);
+super.display();
 
+   }
 }
