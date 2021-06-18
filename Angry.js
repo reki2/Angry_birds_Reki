fbc96ed226aeb2f9cae4739901d795de284d20ec
reki2 <83732645+reki2@users.noreply.github.com>
2021-06-18 16:55:07 -0500

    updata

--- Angry.js
+++ Angry.js
@@ -3,11 +3,22 @@ class Angry extends Plane{
 constructor(x,y){
    super(x,y,50,50);
     this.image = loadImage("sprites/bird.png");
+    this.images = loadImage ("sprites/smoke.png");
+    this.trajectory = []
   }  
 display(){
   /*this.body.position.x = mouseX;
   this.body.position.y = mouseY;*/
   super.display();
+  if(this.body.velocity.x > 8){
+  var pose = [this.body.position.x, this.body.position.y]
+  this.trajectory.push (pose);
+ for(var i = 0; i < this.trajectory.length; i++){
+ image(this.images, this.trajectory[i] [0], this.trajectory[i] [1]);
+ }
+} 
+
+
   fill(220);
 }
 
