4e0327cfffdfed9a644a29609a58c986b463d642
reki2 <83732645+reki2@users.noreply.github.com>
2021-06-21 16:53:31 -0500

    Update Angry.js
    updates

--- Angry.js
+++ Angry.js
@@ -10,12 +10,12 @@ display(){
   /*this.body.position.x = mouseX;
   this.body.position.y = mouseY;*/
   super.display();
-  if(this.body.velocity.x > 8){
+  if(this.body.velocity.x > 8 && this.body.position.x > 195){
   var pose = [this.body.position.x, this.body.position.y]
   this.trajectory.push (pose);
- for(var i = 0; i < this.trajectory.length; i++){
- image(this.images, this.trajectory[i] [0], this.trajectory[i] [1]);
- }
+  for(var i = 0; i < this.trajectory.length; i++){
+    image(this.images, this.trajectory[i] [0], this.trajectory[i] [1]);
+    }
 } 
 
 
